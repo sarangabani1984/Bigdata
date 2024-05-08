522545f42b5b33f451e234fd5175fa2c3fb5abf7
sarangabani1984 <151030032+sarangabani1984@users.noreply.github.com>
2024-05-08 07:39:52 +0530

    Add files via upload

---
+++ emp_management_up.py
@@ -0,0 +1,45 @@
+#OOPS+FBP
+#Default Contructor Class - If we don't mention __init__ function, then it is default constructor
+class ClsSal():#class naming convention is InitUpper case
+    #I didn't used __init__ function here, but still it is internally by default python created and used it
+    #Without this init function, a class cannot be constructed or initialized
+    #def __init__(self):#Init function is used to initialize/construct a class
+#        pass
+    def sal_bon_inc(self,sal,bon,inc):#member/child methods of the given class self argument is needed mandatorily
+        return sal + bon + inc
+    def sal_bon(self,sal,bon):
+        return sal + bon
+
+#Parameterized Contructor Class - We need class variables (parameterized) to use by other members
+
+#Non Parameterized Contructor Class - We need class variables (hardcoded) to use by other members
+class ClsSalDeptNP():#Don't pass a parameter to the class when it get instantiated, but needed some hardcoded class variable to be initialized when class is constructed
+    def __init__(ClsSalDept):#self keyword is the reference created for refering the class
+        ClsSalDept.bon=1000#bon is class member variable, can be accessed by all other members of the clssaldept class
+    def sal_bon(ClsSalDept,sal):#class member method
+        return sal + ClsSalDept.bon#+ClsSalDept.amt_earned_by_init
+
+class ClsLeave():
+    def calc_workdays(self,workingdays):
+        days_to_reduce = 31 - workingdays
+        return days_to_reduce
+
+    def calc_work_sal(self,salperday,workingdays):
+        final_sal = salperday * workingdays
+        return final_sal
+
+
+class ClsHealth():
+    def grp_insurance(self,members_cnt):
+        insurance_amt=members_cnt*10000
+        return insurance_amt
+
+#FBP
+def sal_bon(sal,bon):
+    return sal + bon
+
+
+from python.learning.function_based_prog_3 import simple_calc
+
+
+print(simple_calc(30,40,'add'))
